b45f038ebd7b02ed20c7df22cf12c636f9c9e892
Basu0807 <vasupandeyji.98@gmail.com>
2023-11-29 20:28:31 +0530

    commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const {connection} =require('./Config/db');
 const productRoute = require('./Routes/productRoute');
 const UserRoute = require('./Routes/userRoute');
 const OrderRoute = require('./Routes/orderRoute');
-const stripe =require('stripe')(process.env.SECRET_STRIPE_KEY)
+const stripe=require("stripe")("sk_test_51OHNDNSEW2AXc16ZBAEHpvqalk8PlE6NnomLVaNBKct3I65YWgGqW7vkw8xlhNha0xCXyVOeaISEDIZ52ty1UOcy00BbHHzkqv")
 
 
 const app =express();
@@ -21,11 +21,28 @@ app.use('/store',productRoute)
 app.use('/user',UserRoute)
 app.use('/order',OrderRoute)
 
-app.post('/checkout',async(req,res)=>{
-const PaymentDetails =req.body
-console.log(PaymentDetails);
-})
-
+app.post("/check/checkoutdata",async(req,res)=>{
+  const {products}=req.body
+  console.log(products)
+  const lineItems =products.map((product)=>({
+      price_data:{
+          currency:"inr",
+          product_data:{
+              name:product.heading,
+          },
+          unit_amount:product.price * 100
+      },
+      quantity:product.quantity
+     }))
+     const session =await stripe.checkout.sessions.create({
+      payment_method_types:["card"],
+       line_items:lineItems,
+      mode:"payment",
+      success_url:"https://dainty-pothos-8d469f.netlify.app//success",
+      cancel_url:"https://dainty-pothos-8d469f.netlify.app/cancel",
+  })
+  res.json({id:session.id})
+  })
 app.get('/',(req,res)=>{
 res.send("This is our HomePage")
 console.log("Home page");
